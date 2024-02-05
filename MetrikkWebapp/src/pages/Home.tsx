0b374e013671c230ade83a40dc98daadbe72eb19
DevBilalAhmed <Bilal-a99@outlook.com>
2024-02-05 15:06:45 +0100

    minor changes, changing styling to tailwind layout

--- MetrikkWebapp/src/pages/Home.tsx
+++ MetrikkWebapp/src/pages/Home.tsx
@@ -3,21 +3,19 @@ import "@navikt/ds-css";
 import AreaChartCustomAccessibility
     from "../components/charts/AreaChartCustomAccessibility/AreaChartCustomAccessibility";
 
-
 export default function Home() {
-    return(
+    return (
         <div>
-            <h1>This is the homepage</h1>
-            <div style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                flexDirection: 'row',
-                alignItems: 'center',
-                flexWrap: 'wrap' // This ensures responsiveness
-            }}>
-                <AreaChartCustomAccessibility/>
-                <AreaChartCustomAccessibility/>
+            <h1 className="text-2xl font-bold mb-4">This is the homepage</h1>
+            <div className="flex flex-row justify-between items-center flex-wrap">
+                <div className="">
+                    <AreaChartCustomAccessibility />
+                </div>
+                <div className="">
+                    <AreaChartCustomAccessibility />
+                </div>
             </div>
         </div>
     );
 }
+
