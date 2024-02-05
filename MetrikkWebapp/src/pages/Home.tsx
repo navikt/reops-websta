a2085e46027b248986b1d5789768cf480c664273
DevBilalAhmed <Bilal-a99@outlook.com>
2024-02-05 15:09:09 +0100

    adding todo for what needs changes

--- MetrikkWebapp/src/pages/Home.tsx
+++ MetrikkWebapp/src/pages/Home.tsx
@@ -7,6 +7,7 @@ export default function Home() {
     return (
         <div>
             <h1 className="text-2xl font-bold mb-4">This is the homepage</h1>
+            {/*TODO: Charts er lenger til høyre når de er centered fordi centrering starter på y-axis */}
             <div className="flex flex-row justify-between items-center flex-wrap">
                 <div className="">
                     <AreaChartCustomAccessibility />
