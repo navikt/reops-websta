9a0045b164315db6eca12c12f80b1fe05760e126
DevBilalAhmed <Bilal-a99@outlook.com>
2024-02-08 14:41:49 +0100

    making minor changes to include another chart and centering layout

--- MetrikkWebapp/src/pages/Home.tsx
+++ MetrikkWebapp/src/pages/Home.tsx
@@ -4,11 +4,14 @@ import AreaChartCustomAccessibility
     from "../components/charts/AreaChartCustomAccessibility/AreaChartCustomAccessibility";
 import HorizontalBarChartCustomAccessibility
     from "../components/charts/HorizontalBarChart/HorizontalBarChartCustomAccessibility";
+import {
+    VerticalBarChartCustomAccessibilityExample
+} from "../components/charts/VerticalBarChartCustomAccessibility/VerticalBarChartCustomAccessibility";
 
 export default function Home() {
     return (
         <div>
-            <h1 className="text-2xl font-bold mb-4">This is the homepage</h1>
+            <h1 className="text-2xl font-bold mb-4 text-center">This is the homepage</h1>
             {/*TODO: Charts er lenger til høyre når de er centered fordi centrering starter på y-axis */}
             <div className="flex flex-row justify-between items-center flex-wrap">
                 <div className="">
@@ -19,6 +22,9 @@ export default function Home() {
                 </div>
                 <div className="">
                     <HorizontalBarChartCustomAccessibility />
+                </div>
+                <div className="">
+                    <VerticalBarChartCustomAccessibilityExample />
                 </div>
             </div>
         </div>
