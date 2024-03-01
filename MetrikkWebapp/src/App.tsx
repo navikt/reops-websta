1ece419ce10a951f593b1b05134f48d99696c279
eld-ray <joneldraytamon@gmail.com>
2024-03-01 15:09:46 +0100

    Search bar and Guide Page. Also fixed footer

--- MetrikkWebapp/src/App.tsx
+++ MetrikkWebapp/src/App.tsx
@@ -1,35 +1,35 @@
-import {HashRouter, Route, Routes} from 'react-router-dom';
-import {ContentContainer} from "@navikt/ds-react";
-import './App.css'
-import './index.css'
-import Home from "./pages/Home";
-import {Footer, Nav, ScrollToTop} from "./components/theme";
-
+import { HashRouter, Route, Routes, Link } from 'react-router-dom';
+import { ContentContainer } from '@navikt/ds-react';
+import './App.css';
+import './index.css';
+import Home from './pages/Home';
+import Guide from './pages/Guide';
+import { Footer, Nav, ScrollToTop } from './components/theme';
 
 const routes = [
-    { path: "*", component: <Home /> },
+  { path: '/', component: <Home /> },
+  { path: '/guide', component: <Guide /> },
 ];
 
 function App() {
-
   return (
-     <div>
-        <Nav/>
-         <main style={{maxWidth: "600px", marginLeft: "auto", marginRight: "auto"}}>
-             <ContentContainer>
-                 <HashRouter>
-                     <Routes>
-                         {routes.map(({ path, component }) => (
-                             <Route key={path} path={path} element={component} />
-                         ))}
-                     </Routes>
-                 </HashRouter>
-             </ContentContainer>
-             <ScrollToTop/>
-         </main>
-         <Footer/>
-     </div>
-  )
+    <div className="flex flex-col min-h-screen">
+      <Nav />
+      <main className="flex-grow">
+        <ContentContainer>
+          <HashRouter>
+            <Routes>
+              {routes.map(({ path, component }) => (
+                <Route key={path} path={path} element={component} />
+              ))}
+            </Routes>
+          </HashRouter>
+        </ContentContainer>
+        <ScrollToTop />
+      </main>
+      <Footer className="mt-auto" />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- MetrikkWebapp/src/pages/Home.tsx
+++ MetrikkWebapp/src/pages/Home.tsx
@@ -1,27 +1,43 @@
-import { useState } from 'react';
-import "@navikt/ds-css";
-import AreaChartCustomAccessibility
-    from "../components/charts/AreaChartCustomAccessibility/AreaChartCustomAccessibility";
-import HorizontalBarChartCustomAccessibility
-    from "../components/charts/HorizontalBarChart/HorizontalBarChartCustomAccessibility";
+import '@navikt/ds-css';
+import AreaChartCustomAccessibility from '../components/charts/AreaChartCustomAccessibility/AreaChartCustomAccessibility';
+import HorizontalBarChartCustomAccessibility from '../components/charts/HorizontalBarChart/HorizontalBarChartCustomAccessibility';
+import { Search } from '@navikt/ds-react';
+import { Heading, VStack } from '@navikt/ds-react';
+import { Link } from 'react-router-dom';
 
-export default function Home() {
-    return (
-        <div>
-            <h1 className="text-2xl font-bold mb-4">This is the homepage</h1>
-            {/*TODO: Charts er lenger til høyre når de er centered fordi centrering starter på y-axis */}
-            <div className="flex flex-row justify-between items-center flex-wrap">
-                <div className="">
-                    <AreaChartCustomAccessibility />
-                </div>
-                <div className="">
-                    <AreaChartCustomAccessibility />
-                </div>
-                <div className="">
-                    <HorizontalBarChartCustomAccessibility />
-                </div>
-            </div>
+const Home = () => {
+  const simpleGuide = 'Trykk her for en enkel guide';
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <VStack className="items-center mb-3">
+        <Link to="/guide" className="text-center hover:underline">
+          <Heading size="medium">{simpleGuide}</Heading>
+        </Link>
+        <h2 className="text-xl font-bold mb-3 text-center">Søk her:</h2>
+      </VStack>
+      <form className="w-full max-w-lg px-4">
+        <Search
+          label="Søk alle NAV sine sider"
+          variant="primary"
+          className="w-full"
+        />
+      </form>
+      {/* <div className="flex flex-wrap -mx-2">
+        <div className="w-1/2 px-2">
+          <AreaChartCustomAccessibility />
         </div>
-    );
-}
+        <div className="w-1/2 px-2">
+          <AreaChartCustomAccessibility />
+        </div>
+        <div className="w-1/2 px-2">
+          <HorizontalBarChartCustomAccessibility />
+        </div>
+        <div className="w-1/2 px-2">
+          <HorizontalBarChartCustomAccessibility />
+        </div>
+      </div> */}
+    </div>
+  );
+};
 
+export default Home;
