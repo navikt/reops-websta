627960decb95af6eeaaf42ae0528b148612d7742
eld-ray <joneldraytamon@gmail.com>
2024-03-04 15:19:03 +0100

    Update test to contain hashrouter

--- MetrikkWebapp/src/pages/Home.tsx
+++ MetrikkWebapp/src/pages/Home.tsx
@@ -4,16 +4,19 @@ import HorizontalBarChartCustomAccessibility from '../components/charts/Horizont
 import { Search } from '@navikt/ds-react';
 import { Heading, VStack } from '@navikt/ds-react';
 import { Link } from 'react-router-dom';
-import {VerticalBarChartCustomAccessibilityExample} from "../components/charts/VerticalBarChartCustomAccessibility/VerticalBarChartCustomAccessibility";
-import {fetchAmplitudeData} from "../service/AmplitudeApi";
+import { VerticalBarChartCustomAccessibilityExample } from '../components/charts/VerticalBarChartCustomAccessibility/VerticalBarChartCustomAccessibility';
+import { fetchAmplitudeData } from '../service/AmplitudeApi';
 
-import AreaChartContainer from "../components/charts/AreaChartCustomAccessibility/AreaChartContainer";
+import AreaChartContainer from '../components/charts/AreaChartCustomAccessibility/AreaChartContainer';
 
 const Home = () => {
   const simpleGuide = 'Trykk her for en enkel guide';
-    
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold mb-4 text-center">
+        This is the homepage
+      </h1>
       <VStack className="items-center mb-3">
         <Link to="/guide" className="text-center hover:underline">
           <Heading size="medium">{simpleGuide}</Heading>
@@ -27,22 +30,21 @@ const Home = () => {
           className="w-full"
         />
       </form>
-        <h1 className="text-2xl font-bold mb-4 text-center">This is the homepage</h1>
-            {/*TODO: Charts er lenger til høyre når de er centered fordi centrering starter på y-axis */}
-            <div className="flex flex-row justify-between items-center flex-wrap">
-                <div className="">
-                    <AreaChartContainer />
-                </div>
-                <div className="">
-                    <AreaChartContainer />
-                </div>
-                <div className="">
-                    <HorizontalBarChartCustomAccessibility />
-                </div>
-                <div className="">
-                    <VerticalBarChartCustomAccessibilityExample />
-                </div> 
+      {/*TODO: Charts er lenger til høyre når de er centered fordi centrering starter på y-axis */}
+      <div className="flex flex-row justify-between items-center flex-wrap">
+        <div className="">
+          <AreaChartContainer />
         </div>
+        <div className="">
+          <AreaChartContainer />
+        </div>
+        <div className="">
+          <HorizontalBarChartCustomAccessibility />
+        </div>
+        <div className="">
+          <VerticalBarChartCustomAccessibilityExample />
+        </div>
+      </div>
     </div>
   );
 };
