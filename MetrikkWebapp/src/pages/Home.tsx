6bbb5eefd4031ed690b8a0360534d0e028463011
eld-ray <joneldraytamon@gmail.com>
2024-04-18 13:33:16 +0200

    Fix grid and background color

--- MetrikkWebapp/src/pages/Home.tsx
+++ MetrikkWebapp/src/pages/Home.tsx
@@ -5,34 +5,36 @@ import { Search } from '@navikt/ds-react';
 import { Heading, VStack } from '@navikt/ds-react';
 import { Link } from 'react-router-dom';
 import { VerticalBarChartCustomAccessibilityExample } from '../components/charts/VerticalBarChartCustomAccessibility/VerticalBarChartCustomAccessibility';
-import {Test} from "../components/charts/AreaChartCustomAccessibility/test";
-import DisplayTableChart from "../components/charts/TableChart/DisplayTableChart.tsx";
-import TableChart from "../components/charts/TableChart/TableChart.tsx";
+import { Test } from '../components/charts/AreaChartCustomAccessibility/test';
+import DisplayTableChart from '../components/charts/TableChart/DisplayTableChart.tsx';
+import TableChart from '../components/charts/TableChart/TableChart.tsx';
 
 import AreaChartContainer from '../components/charts/AreaChartCustomAccessibility/AreaChartContainer';
-import {eventTypeMappings} from "../components/charts/fetchUrlConstructor";
-import {SearchComponent} from "../components/SearchComponent/SearchComponent.tsx";
-import {SetStateAction, useCallback, useState} from "react";
+import { eventTypeMappings } from '../components/charts/fetchUrlConstructor';
+import { SearchComponent } from '../components/SearchComponent/SearchComponent.tsx';
+import { SetStateAction, useCallback, useState } from 'react';
+import { HGrid } from '@navikt/ds-react';
 
 const Home = () => {
-    const simpleGuide = 'Trykk her for en enkel guide';
+  const simpleGuide = 'Trykk her for en enkel guide';
 
-    // Kan hende callback blir brukt til å velge domene
-    const [selectedDomain, setSelectedDomain] = useState("100002286");
+  // Kan hende callback blir brukt til å velge domene
+  const [selectedDomain, setSelectedDomain] = useState('100002286');
 
-    const handleDomainSelect = useCallback((domain) => {
-        setSelectedDomain(domain);
-    }, []);
+  const handleDomainSelect = useCallback((domain) => {
+    setSelectedDomain(domain);
+  }, []);
 
-    console.log(`from home`,selectedDomain)
+  console.log(`from home`, selectedDomain);
 
-    return (
+  return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-2xl font-bold mb-4 text-center">
         This is the homepage
       </h1>
-      <SearchComponent onDomainSelect={handleDomainSelect}/>
-        {/*<VStack className="items-center mb-3">
+      <SearchComponent onDomainSelect={handleDomainSelect} />
+      <div className="h-8"></div>
+      {/*<VStack className="items-center mb-3">
         <Link to="/guide" className="text-center hover:underline">
           <Heading size="medium">{simpleGuide}</Heading>
         </Link>
@@ -47,150 +49,144 @@ const Home = () => {
       </form>
       */}
       {/*TODO: Charts er lenger til høyre når de er centered fordi centrering starter på y-axis */}
-        <div className="flex flex-row justify-between items-center flex-wrap">
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="p-4 bg-white border border-blue-200 rounded shadow-lg">
+          <AreaChartContainer
+            teamDomain={selectedDomain}
+            chartType="areaChartMulti"
+            endpointType="segmentation"
+            urlParams={{
+              startDate: '20240101',
+              endDate: '20240130',
+              eventType: eventTypeMappings.pageViewed,
+            }}
+          />
+        </div>
 
-            <div className="">
-                <AreaChartContainer
-                    teamDomain={selectedDomain}
-                    chartType="areaChartMulti"
-                    endpointType="segmentation"
-                    urlParams={{
-                        startDate: "20240101",
-                        endDate: "20240130",
-                        eventType: eventTypeMappings.pageViewed
-                    }}
-                />
-            </div>
+        <div className="p-4 bg-white border border-blue-200 rounded shadow-lg">
+          <AreaChartContainer
+            teamDomain={selectedDomain}
+            chartType="areaChartMulti"
+            endpointType="segmentation"
+            urlParams={{
+              startDate: '20240101',
+              endDate: '20240130',
+              eventType: eventTypeMappings.pageViewedGroupByReferrer,
+            }}
+          />
+        </div>
+        <div className="p-4 bg-white border border-blue-200 rounded shadow-lg">
+          <AreaChartContainer
+            teamDomain={selectedDomain}
+            chartType="areaChartMulti"
+            endpointType="segmentation"
+            urlParams={{
+              startDate: '20240101',
+              endDate: '20240130',
+              eventType: eventTypeMappings.pageViewedGroupByPagePath,
+            }}
+          />
+        </div>
+        <div className="p-4 bg-white border border-blue-200 rounded shadow-lg">
+          <AreaChartContainer
+            teamDomain={selectedDomain}
+            chartType="areaChartMulti"
+            endpointType="segmentation"
+            urlParams={{
+              startDate: '20240101',
+              endDate: '20240130',
+              eventType: eventTypeMappings.pageViewedGroupByCity,
+            }}
+          />
+        </div>
+        <div className="p-4 bg-white border border-blue-200 rounded shadow-lg">
+          <AreaChartContainer
+            teamDomain={selectedDomain}
+            chartType="areaChartMulti"
+            endpointType="segmentation"
+            urlParams={{
+              startDate: '20240101',
+              endDate: '20240130',
+              eventType: eventTypeMappings.pageViewedGroupByLanguage,
+            }}
+          />
+        </div>
+        <div className="p-4 bg-white border border-blue-200 rounded shadow-lg">
+          <AreaChartContainer
+            teamDomain={selectedDomain}
+            chartType="areaChartMulti"
+            endpointType="segmentation"
+            urlParams={{
+              startDate: '20240101',
+              endDate: '20240130',
+              eventType: eventTypeMappings.pageViewedGroupByCountry,
+            }}
+          />
+        </div>
 
+        <div className="p-4 bg-white border border-blue-200 rounded shadow-lg">
+          <AreaChartContainer
+            teamDomain={selectedDomain}
+            chartType="areaChartMulti"
+            endpointType="segmentation"
+            urlParams={{
+              startDate: '20240101',
+              endDate: '20240130',
+              eventType: eventTypeMappings.pageViewedGroupByDayOfWeek,
+            }}
+          />
+        </div>
 
-            <div className="">
-                <AreaChartContainer
-                    teamDomain={selectedDomain}
-                    chartType="areaChartMulti"
-                    endpointType="segmentation"
-                    urlParams={{
-                        startDate: "20240101",
-                        endDate: "20240130",
-                        eventType: eventTypeMappings.pageViewedGroupByReferrer
-                    }}
-                />
-            </div>
-            <div className="">
-                <AreaChartContainer
-                    teamDomain={selectedDomain}
-                    chartType="areaChartMulti"
-                    endpointType="segmentation"
-                    urlParams={{
-                        startDate: "20240101",
-                        endDate: "20240130",
-                        eventType: eventTypeMappings.pageViewedGroupByPagePath
-                    }}
-                />
-            </div>
-            <div className="">
-                <AreaChartContainer
-                    teamDomain={selectedDomain}
-                    chartType="areaChartMulti"
-                    endpointType="segmentation"
-                    urlParams={{
-                        startDate: "20240101",
-                        endDate: "20240130",
-                        eventType: eventTypeMappings.pageViewedGroupByCity
-                    }}
-                />
-            </div>
-            <div className="">
-                <AreaChartContainer
-                    teamDomain={selectedDomain}
-                    chartType="areaChartMulti"
-                    endpointType="segmentation"
-                    urlParams={{
-                        startDate: "20240101",
-                        endDate: "20240130",
-                        eventType: eventTypeMappings.pageViewedGroupByLanguage
-                    }}
-                />
-            </div>
-            <div className="">
-                <AreaChartContainer
-                    teamDomain={selectedDomain}
-                    chartType="areaChartMulti"
-                    endpointType="segmentation"
-                    urlParams={{
-                        startDate: "20240101",
-                        endDate: "20240130",
-                        eventType: eventTypeMappings.pageViewedGroupByCountry
-                    }}
-                />
-            </div>
-
-
-            <div className="">
-                <AreaChartContainer
-                    teamDomain={selectedDomain}
-                    chartType="areaChartMulti"
-                    endpointType="segmentation"
-                    urlParams={{
-                        startDate: "20240101",
-                        endDate: "20240130",
-                        eventType: eventTypeMappings.pageViewedGroupByDayOfWeek
-                    }}
-                />
-            </div>
-
-
-
-            {/*
-        <div className="">
+        {/*
+        <div className="p-4 bg-white border border-blue-200 rounded shadow-lg">
           <HorizontalBarChartCustomAccessibility />
         </div>
-        <div className="">
+        <div className="p-4 bg-white border border-blue-200 rounded shadow-lg">
           <VerticalBarChartCustomAccessibilityExample />
         </div>
         */}
 
-            <div className="">
-                <AreaChartContainer
-                    teamDomain={selectedDomain}
-                    chartType="areaChartMulti"
-                    endpointType="segmentation"
-                    urlParams={{
-                        startDate: "20240101",
-                        endDate: "20240130",
-                        eventType: eventTypeMappings.pageViewedGroupByHourOfDay
-                    }}
-                />
-            </div>
-            <div className="">
-                <AreaChartContainer
-                    teamDomain={selectedDomain}
-                    chartType="areaChartMulti"
-                    endpointType="segmentation"
-                    urlParams={{
-                        startDate: "20240309",
-                        endDate: "20240408",
-                        eventType: eventTypeMappings.pageViewedGroupByCity
-                    }}
-                />
-            </div>
-
-            <div className="">
-                <AreaChartContainer
-                    teamDomain={selectedDomain}
-                    chartType="retentionChart"
-                    endpointType="retention"
-                    urlParams={{
-                        startDate: "20240205",
-                        endDate: "20240304",
-                        eventType: eventTypeMappings.pageViewed,
-                        secondEventType: eventTypeMappings.pageViewed
-                    }}
-                />
-            </div>
-
-
-            {/* <Test/> */}
+        <div className="p-4 bg-white border border-blue-200 rounded shadow-lg">
+          <AreaChartContainer
+            teamDomain={selectedDomain}
+            chartType="areaChartMulti"
+            endpointType="segmentation"
+            urlParams={{
+              startDate: '20240101',
+              endDate: '20240130',
+              eventType: eventTypeMappings.pageViewedGroupByHourOfDay,
+            }}
+          />
         </div>
+        <div className="p-4 bg-white border border-blue-200 rounded shadow-lg">
+          <AreaChartContainer
+            teamDomain={selectedDomain}
+            chartType="areaChartMulti"
+            endpointType="segmentation"
+            urlParams={{
+              startDate: '20240309',
+              endDate: '20240408',
+              eventType: eventTypeMappings.pageViewedGroupByCity,
+            }}
+          />
+        </div>
+
+        <div className="p-4 bg-white border border-blue-200 rounded shadow-lg">
+          <AreaChartContainer
+            teamDomain={selectedDomain}
+            chartType="retentionChart"
+            endpointType="retention"
+            urlParams={{
+              startDate: '20240205',
+              endDate: '20240304',
+              eventType: eventTypeMappings.pageViewed,
+              secondEventType: eventTypeMappings.pageViewed,
+            }}
+          />
+        </div>
+
+        {/* <Test/> */}
+      </div>
     </div>
   );
 };
