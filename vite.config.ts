d16c864fc1c9b26685d34480748abf8a020bdc23
EIlif Johansen <eilifjohansen@outlook.com>
2024-05-09 01:46:12 +0200

    La til port 8080 i vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    reporters: ['verbose','html'],
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html']
+  server: {
+    watch: {
+      usePolling: true,
     },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 8080, // you can replace this port with any port
   },
-})
+});
