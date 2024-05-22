43e9067c2aa64799e37427ac61f0605a88236a69
EIlif Johansen <eilifjohansen@outlook.com>
2024-05-22 15:03:45 +0200

    Fikset service filer

--- src/service/AmplitudeApi.tsx
+++ src/service/AmplitudeApi.tsx
@@ -7,8 +7,13 @@ export const fetchAmplitudeData = async (endpoint:string, teamDomain:string) => 
         const response = await fetch(`${BASE_URL}${teamUrl}${endpoint}`, {
             credentials: 'include'
         });
-        // rest of the code
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return await response.json();
     } catch (error) {
-        // error handling
+        // Handle or throw the error
+        console.error("Error during fetch: ", error);
+        throw error;
     }
 };
--- src/service/SiteimproveApi.tsx
+++ src/service/SiteimproveApi.tsx
@@ -2,11 +2,15 @@ const BASE_URL = 'https://reops-proxy.ansatt.nav.no/siteimprove';
 
 export const fetchSiteimproveData = async (endpoint: string) => {
     try {
-        const response = await fetch(`${BASE_URL}${endpoint}`, {
+        const response = await fetch(`${BASE_URL}${teamUrl}${endpoint}`, {
             credentials: 'include'
         });
-        // rest of the code
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return await response.json();
     } catch (error) {
-        // error handling
+        console.error("Error during fetch: ", error);
+        throw error;
     }
 };
