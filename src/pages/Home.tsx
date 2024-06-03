c14815758933ad222ddacf9bfc198c752028421b
EIlif Johansen <eilifjohansen@outlook.com>
2024-06-03 11:40:46 +0200

    Språkfiks

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -166,8 +166,9 @@ const Home = () => {
               />
             </div>
             {!selectedDomain && (
-                <Alert variant="info" className="mt-6">ch
-                  For å sikre at du ser korrekt statistikk, anbefaler vi at du kopierer og limer inn lenken. Eksempel: https://www.nav.no (forsiden til NAV).
+                <Alert variant="info" className="mt-6">
+                  For å sikre at du ser korrekt statistikk, anbefaler vi at du kopierer og limer inn lenken. <br/><br />Eksempel:
+                  https://www.nav.no (forsiden til NAV).
                 </Alert>
             )}
           </div>
