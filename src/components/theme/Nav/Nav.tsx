26296c3e3b1d8623a2eeb27df0cdfa184480a96b
EIlif Johansen <eilifjohansen@outlook.com>
2024-06-08 23:33:12 +0200

    Justerete logo plassering

--- src/components/theme/Nav/Nav.tsx
+++ src/components/theme/Nav/Nav.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 function Nav() {
   return (
     <nav className="fixed top-0 left-0  w-full bg-white z-10 shadow-md">
-      <div className="py-5 px-6 flex justify-between items-center">
-        <a href="." className="font-bold text-xl">
+      <div className="py-4 px-6 flex justify-between items-center">
+        <a href="." className="font-bold text-2xl">
           Innblikk
         </a>
       </div>
