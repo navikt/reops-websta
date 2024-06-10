313921d7c459b3fff647051a3cb03d8164541c64
EIlif Johansen <eilifjohansen@outlook.com>
2024-06-10 15:31:37 +0200

    Forbedret skjuling av Siteimprove før den er klar

--- src/components/Siteimprove/SiteScores.tsx
+++ src/components/Siteimprove/SiteScores.tsx
@@ -82,112 +82,116 @@ const SiteScores = ({pageUrl, siteimproveSelectedDomain}) => {
     };
 
     return (
-        <div className="p-4 w-full bg-white border border-blue-200 rounded shadow-lg md:col-span-2 mb-6">
-            <div className="w-full">
-                <div className="mt-2 bg-white p-4 rounded-lg">
-                    <h2 className="text-xl font-semibold mb-2" style={{color: '#000000'}}>
-                        Poengsum (av 100)
-                    </h2>
-                    <hr className="my-4 border-t-2 border-gray-300"/>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-                        {scoreOverview && (
-                            <div className="flex flex-col items-center justify-center w-full p-4">
-                                <div className="w-40 h-40">
-                                    <CircularProgressbar
-                                        className="text-xl font-bold"
-                                        value={scoreOverview.qa.total}
-                                        text={`${roundToOneDecimal(scoreOverview.qa.total)}`}
-                                        aria-label="Kvalitetsikring av innhold score"
-                                        styles={buildStyles({
-                                            pathColor: getColor(scoreOverview.qa.total),
-                                            textColor: '#000000',
-                                        })}
-                                    />
-                                </div>
-                                <h3 className="text-center mt-3 font-bold text-xl">{`Kvalitetsikring av innhold`}</h3>
+        <>
+            {scoreOverview && (
+                <div className="p-4 w-full bg-white border border-blue-200 rounded shadow-lg md:col-span-2 mb-6">
+                    <div className="w-full">
+                        <div className="mt-2 bg-white p-4 rounded-lg">
+                            <h2 className="text-xl font-semibold mb-2" style={{color: '#000000'}}>
+                                Poengsum (av 100)
+                            </h2>
+                            <hr className="my-4 border-t-2 border-gray-300"/>
+                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                                {scoreOverview && (
+                                    <div className="flex flex-col items-center justify-center w-full p-4">
+                                        <div className="w-40 h-40">
+                                            <CircularProgressbar
+                                                className="text-xl font-bold"
+                                                value={scoreOverview.qa.total}
+                                                text={`${roundToOneDecimal(scoreOverview.qa.total)}`}
+                                                aria-label="Kvalitetsikring av innhold score"
+                                                styles={buildStyles({
+                                                    pathColor: getColor(scoreOverview.qa.total),
+                                                    textColor: '#000000',
+                                                })}
+                                            />
+                                        </div>
+                                        <h3 className="text-center mt-3 font-bold text-xl">{`Kvalitetsikring av innhold`}</h3>
+                                    </div>
+                                )}
+                                {scoreOverview && (
+                                    <div className="flex flex-col items-center justify-center w-full p-4">
+                                        <div className="w-40 h-40">
+                                            <CircularProgressbar
+                                                className="text-xl font-bold"
+                                                value={scoreOverview.a11y.total}
+                                                text={`${roundToOneDecimal(scoreOverview.a11y.total)}`}
+                                                aria-label="Universell utforming score"
+                                                styles={buildStyles({
+                                                    pathColor: getColor(scoreOverview.a11y.total),
+                                                    textColor: '#000000',
+                                                })}
+                                            />
+                                        </div>
+                                        <h3 className="text-center mt-3 font-bold text-xl">{`Universell utforming`}</h3>
+                                    </div>
+                                )}
+                                {scoreOverview && (
+                                    <div className="flex flex-col items-center justify-center w-full p-4">
+                                        <div className="w-40 h-40">
+                                            <CircularProgressbar
+                                                className="text-xl font-bold"
+                                                value={scoreOverview.seo.total}
+                                                text={`${roundToOneDecimal(scoreOverview.seo.total)}`}
+                                                aria-label="Søkemotor-optimal score"
+                                                styles={buildStyles({
+                                                    pathColor: getColor(scoreOverview.seo.total),
+                                                    textColor: '#000000',
+                                                })}
+                                            />
+                                        </div>
+                                        <h3 className="text-center mt-3 font-bold text-xl">{`Søkemotoroptimalisering`}</h3>
+                                    </div>
+                                )}
+                                {scoreOverview && (
+                                    <div className="flex flex-col items-center justify-center w-full p-4">
+                                        <div className="w-40 h-40">
+                                            <CircularProgressbar
+                                                className="text-xl font-bold"
+                                                value={scoreOverview.total}
+                                                text={`${roundToOneDecimal(scoreOverview.total)}`}
+                                                aria-label="Total poengsum"
+                                                styles={buildStyles({
+                                                    pathColor: getColor(scoreOverview.total),
+                                                    textColor: '#000000',
+                                                })}
+                                            />
+                                        </div>
+                                        <h3
+                                            className="text-center mt-3 font-bold text-lg"
+                                            style={{color: '#000000'}}
+                                        >{`Snittpoeng`}</h3>
+                                    </div>
+                                )}
                             </div>
-                        )}
-                        {scoreOverview && (
-                            <div className="flex flex-col items-center justify-center w-full p-4">
-                                <div className="w-40 h-40">
-                                    <CircularProgressbar
-                                        className="text-xl font-bold"
-                                        value={scoreOverview.a11y.total}
-                                        text={`${roundToOneDecimal(scoreOverview.a11y.total)}`}
-                                        aria-label="Universell utforming score"
-                                        styles={buildStyles({
-                                            pathColor: getColor(scoreOverview.a11y.total),
-                                            textColor: '#000000',
-                                        })}
-                                    />
-                                </div>
-                                <h3 className="text-center mt-3 font-bold text-xl">{`Universell utforming`}</h3>
-                            </div>
-                        )}
-                        {scoreOverview && (
-                            <div className="flex flex-col items-center justify-center w-full p-4">
-                                <div className="w-40 h-40">
-                                    <CircularProgressbar
-                                        className="text-xl font-bold"
-                                        value={scoreOverview.seo.total}
-                                        text={`${roundToOneDecimal(scoreOverview.seo.total)}`}
-                                        aria-label="Søkemotor-optimal score"
-                                        styles={buildStyles({
-                                            pathColor: getColor(scoreOverview.seo.total),
-                                            textColor: '#000000',
-                                        })}
-                                    />
-                                </div>
-                                <h3 className="text-center mt-3 font-bold text-xl">{`Søkemotoroptimalisering`}</h3>
-                            </div>
-                        )}
-                        {scoreOverview && (
-                            <div className="flex flex-col items-center justify-center w-full p-4">
-                                <div className="w-40 h-40">
-                                    <CircularProgressbar
-                                        className="text-xl font-bold"
-                                        value={scoreOverview.total}
-                                        text={`${roundToOneDecimal(scoreOverview.total)}`}
-                                        aria-label="Total poengsum"
-                                        styles={buildStyles({
-                                            pathColor: getColor(scoreOverview.total),
-                                            textColor: '#000000',
-                                        })}
-                                    />
-                                </div>
-                                <h3
-                                    className="text-center mt-3 font-bold text-lg"
-                                    style={{color: '#000000'}}
-                                >{`Snittpoeng`}</h3>
-                            </div>
-                        )}
-                    </div>
-                    <hr className="my-4 border-t-2 border-gray-300"/>
-                    {reportLink && (
-                        <div className="bg-white pl-0 pt-4 ">
-                            {/*              <h3 className="font-bold text-xl text-center">
+                            <hr className="my-4 border-t-2 border-gray-300"/>
+                            {reportLink && (
+                                <div className="bg-white pl-0 pt-4 ">
+                                    {/*              <h3 className="font-bold text-xl text-center">
                 Siteimprove rapport
               </h3>*/}
-                            <div
-                                className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
-                                <a href={reportLink} target="_blank" rel="noopener noreferrer">
-                                    <p className="text-xl font-bold underline mb-2 text-blue-700">
-                                        Rapport med forklaringer (🔐)
-                                    </p>
-                                </a>
-                                <a href="https://jira.adeo.no/plugins/servlet/desk/portal/581/create/2641"
-                                   target="_blank"
-                                   rel="noopener noreferrer">
-                                    <p className="text-xl  font-bold underline text-blue-700">
-                                        Få Siteimprove tilgang
-                                    </p>
-                                </a>
-                            </div>
+                                    <div
+                                        className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
+                                        <a href={reportLink} target="_blank" rel="noopener noreferrer">
+                                            <p className="text-xl font-bold underline mb-2 text-blue-700">
+                                                Rapport med forklaringer (🔐)
+                                            </p>
+                                        </a>
+                                        <a href="https://jira.adeo.no/plugins/servlet/desk/portal/581/create/2641"
+                                           target="_blank"
+                                           rel="noopener noreferrer">
+                                            <p className="text-xl  font-bold underline text-blue-700">
+                                                Få Siteimprove tilgang
+                                            </p>
+                                        </a>
+                                    </div>
+                                </div>
+                            )}
                         </div>
-                    )}
+                    </div>
                 </div>
-            </div>
-        </div>
+            )}
+        </>
     );
 };
 
